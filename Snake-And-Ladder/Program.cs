470cd56998a2fbf308b05a37007c13b08a469b47
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-28 14:14:00 +0530

    [Amit] Repeat Till Playear Reaches Winning Position 100

--- Snake-And-Ladder/Program.cs
+++ Snake-And-Ladder/Program.cs
@@ -7,25 +7,32 @@
             int position = 0;
             Console.WriteLine($"Player current position is {position}");
             Random random = new Random();
-            int diceRoll = random.Next(1, 7);
-            Console.WriteLine($"The value come after rolling dice is {diceRoll}");
-            int option = random.Next(1, 4);
+            
+            
+            
             SnakeAndLadderBuilder snakeAndLadder = new SnakeAndLadderBuilder();
-            switch (option)
+            while (position<100)
             {
-                case 1:
-                    position = snakeAndLadder.NoPlay(position);
-                    Console.WriteLine("Player position now is " + position);
-                    break;
-                case 2:
-                    position = snakeAndLadder.Ladder(position, diceRoll);
-                    Console.WriteLine("Player position now is " + position);
-                    break;
-                default:
-                    position = snakeAndLadder.Snake(position, diceRoll);
-                    Console.WriteLine("Player position now is " + position);
-                    break;
+                int diceRoll = random.Next(1, 7);
+                Console.WriteLine($"The value come after rolling dice is {diceRoll}");
+                int option = random.Next(1, 4);
+                switch (option)
+                {
+                    case 1:
+                        position = snakeAndLadder.NoPlay(position);
+                        Console.WriteLine("Player position now is " + position);
+                        break;
+                    case 2:
+                        position = snakeAndLadder.Ladder(position, diceRoll);
+                        Console.WriteLine("Player position now is " + position);
+                        break;
+                    default:
+                        position = snakeAndLadder.Snake(position, diceRoll);
+                        Console.WriteLine("Player position now is " + position);
+                        break;
+                }
             }
+            
 
         }
     }
