ce1dc76819fc32ce5c3d16f0cb1a4c3b84be6c74
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-28 13:40:27 +0530

    [Amit] Single player At Start Position Zero

---
+++ Snake-And-Ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Snake_And_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeAndLadderBuilder snakeAndLadder = new SnakeAndLadderBuilder();
+            snakeAndLadder.GetPosition();
+
+        }
+    }
+}
---
+++ Snake-And-Ladder/SnakeAndLadderBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    public class SnakeAndLadderBuilder
+    {
+        public void GetPosition()
+        {
+            int position = 0;
+            Console.WriteLine($"Player current position is {position}");
+        }
+    }
+}
